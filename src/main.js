02e6843e8678387108393b9d0e4b5ca2ef9b6900
xingfujiang <18224518307@163.com>
2023-08-28 16:44:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -31,3 +31,92 @@ if (historySongs.length > 0) {
 createApp(App).use(store).use(router).use(lazyPlugin, {
   loading: require('@/assets/images/default.png')
 }).directive('loading', loadingDirective).directive('no-result', noResultDirective).mount('#app')
+
+
+let _Vue = null
+
+export default class VueRouter{
+   static install(Vue){
+     if(VueRouter.install.installed){
+         return
+     }
+
+     VueRouter.install.installed = true
+
+     _Vue = Vue
+
+     _Vue.mixin({
+        beforeCreate(){
+          if(this.$options.router){
+            _Vue.prototype.$router = this.$options.router
+            this.$options.router.init()
+          }
+        }
+     })
+  }
+
+   constructor(options) {
+      this.options = options
+
+      this.routeMap = {}
+
+      this.data = _Vue.observable({
+         current:'/'
+      })
+   }
+
+   init(){
+     this.createRouteMap()
+     this.initComponents(_Vue)
+     this.initEvent()
+   }
+
+   createRouteMap(){
+      this.options.routes.forEach(route=>{
+          this.routeMap[route.path] = route.component
+      })
+   }
+
+   initComponents(Vue){
+     Vue.component('router-link',{
+        props:{
+          to:String
+        },
+        render(h){
+           return h('a',{
+             attrs:{
+               href:this.to
+             },
+             on:{
+               click: this.clickHandler
+             }
+           }, [this.$slots.default])
+        },
+        methods:{
+           clickHandler(e){
+              history.pushState({},'',this.to)
+              this.$router.data.current = this.to
+              e.preventDefault()
+           }
+        }
+       // template:'<a :href="to"><slot></slot></a>'
+     })
+
+     const  self = this
+
+     Vue.component('router-view', {
+        render(h){
+         const component = self.routeMap[self.data.current]
+           return h(component)
+        }
+     })
+   }
+
+   initEvent(){
+      window.addEventListener('popstate',()=>{
+
+        this.data.current = window.location.pathname
+      })
+   }
+
+}
